cb9ff658c9aa257883c396609370665c4253ceae
AbdElrahman Gbr <abdelrahman.gbr.os@gmail.com>
2022-08-27 21:09:22 +0200

    wraping up (fixing filter>tag issue, added clear button in filter bar)

--- src/App.js
+++ src/App.js
@@ -21,31 +21,37 @@ function App() {
         if (languages) {
             tags.push(...languages);
         }
-        return tags.some(tag => filters.includes(tag));
+        return filters.every(filter => tags.includes(filter));
     };
 
     const handleTagClick = (tag) => {
         if (filters.includes(tag)) return;
         setFilters([...filters, tag]);
-    }
+    };
+    const clearFilters = () =>{
+        setFilters([]);
+    };
 
     const filteredJobs = jobs.filter(filterFunc);
     const handleFilterClick = (passedFilter) => {
         setFilters(filters.filter(f => f !== passedFilter));
-    }
+    };
+
     return (
         <>
             <Header/>
+            <div className='container m-auto'>
             {
                 filters.length > 0 && (
-                    <div className='bg-white flex shadow-md my-16 mx-10 p-6 rounded'>
+                    <div className='bg-white flex shadow-md -my-20 mb-16 mx-10 p-6 rounded z-10 relative'>
 
                         {filters.map((filter) => <span onClick={() => handleFilterClick(filter)}
-                                                       className='cursor-pointer text-teal-500 bg-teal-100 font-bold mr-4 mb-4 rounded sm:mb-0'>
+                                                       className='cursor-pointer text-teal-500 bg-teal-100 font-bold mr-4 mb-4 rounded lg:mb-0'>
                             <span className='text-teal-500 bg-teal-100 p-2'>{filter}</span>
                             <span className='bg-teal-500 text-teal-100 p-2'>✕</span>
                             </span>
                         )}
+                        <button onClick={clearFilters} className='font-bold text-gray-700 ml-auto'>Clear</button>
                     </div>
                 )}
             {jobs.length === 0 ? (
@@ -53,6 +59,8 @@ function App() {
             ) : (
                 filteredJobs.map((job) => <JobBoard job={job} key={job.id} handleTagClick={handleTagClick}/>)
             )}
+            </div>
+
         </>
     );
 }
