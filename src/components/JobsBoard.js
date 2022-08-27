6990a197d42d8caa342476e6b5fd09fae621dec7
AbdElrahman Gbr <abdelrahman.gbr.os@gmail.com>
2022-08-27 19:05:23 +0200

    made JobBoard responsive

--- src/components/JobsBoard.js
+++ src/components/JobsBoard.js
@@ -24,20 +24,20 @@ const JobBoard = ({
         tags.push(...tools);
     }
     return (<>
-        <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${featured && 'border-l-4 border-teal-500 border-solid'}`}>
+        <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${featured && 'border-l-4 border-teal-500 border-solid'} sm:flex-row`}>
             <div>
-                <img className='-mt-16 mb-4 w-20 h-20' src={logo} alt={company}/>
+                <img className='-mt-16 mb-4 w-20 h-20 sm:h-24 sm:w-24 sm:my-0' src={logo} alt={company}/>
             </div>
             <div className='flex flex-col justify-between ml-4'>
                 <h3 className='font-bold text-xl text-teal-500'>{company} {isNew && (
-                    <span className='bg-teal-500 text-teal-100 font-bold rounded-full m-2 py-1 px-2'>New</span>)} {featured && (
-                    <span className='bg-gray-800 text-white font-bold rounded-full py-1 px-2'>Featured</span>)}</h3>
+                    <span className='bg-teal-500 text-teal-100 font-bold rounded-full m-2 py-1 px-2 uppercase text-sm'>New</span>)} {featured && (
+                    <span className='bg-gray-800 text-white font-bold rounded-full py-1 px-2 uppercase text-sm'>Featured</span>)}</h3>
                 <h2 className="font-bold text-xl my-2">{position}</h2>
                 <p className="text-gray-500 font-medium">
                     {postedAt} · {contract} · {location}
                 </p>
             </div>
-            <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid">
+            <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid sm:ml-auto sm:border-0 sm:mt-0 sm:pt-0 sm:mb-0">
                 {tags ? tags.map((tag) => <span
                     className='text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded'>{tag}</span>) : ''}
             </div>
