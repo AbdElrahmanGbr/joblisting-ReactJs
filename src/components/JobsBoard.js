f15fcfb2eed2e8ae4f5b6d70aa1e904453dac73f
AbdElrahman Gbr <abdelrahman.gbr.os@gmail.com>
2022-08-27 18:34:27 +0200

    creating the mobile view

--- src/components/JobsBoard.js
+++ src/components/JobsBoard.js
@@ -24,22 +24,22 @@ const JobBoard = ({
         tags.push(...tools);
     }
     return (<>
-        <div className={`flex bg-white shadow-md m-4 p-6 rounded ${featured && 'border-l-4 border-teal-500 border-solid'}`}>
+        <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${featured && 'border-l-4 border-teal-500 border-solid'}`}>
             <div>
-                <img src={logo} alt={company}/>
+                <img className='-mt-16 mb-4 w-20 h-20' src={logo} alt={company}/>
             </div>
             <div className='flex flex-col justify-between ml-4'>
                 <h3 className='font-bold text-xl text-teal-500'>{company} {isNew && (
                     <span className='bg-teal-500 text-teal-100 font-bold rounded-full m-2 py-1 px-2'>New</span>)} {featured && (
                     <span className='bg-gray-800 text-white font-bold rounded-full py-1 px-2'>Featured</span>)}</h3>
-                <h2 className="font-bold text-xl">{position}</h2>
+                <h2 className="font-bold text-xl my-2">{position}</h2>
                 <p className="text-gray-500 font-medium">
                     {postedAt} · {contract} · {location}
                 </p>
             </div>
-            <div className="flex justify-end ml-auto items-center">
+            <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid">
                 {tags ? tags.map((tag) => <span
-                    className='text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded'>{tag}</span>) : ''}
+                    className='text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded'>{tag}</span>) : ''}
             </div>
         </div>
 
